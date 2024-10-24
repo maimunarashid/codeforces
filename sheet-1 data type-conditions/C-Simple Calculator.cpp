693b81e857e8f4cde1b2b50df6bfcb860d984582
maimunarashid <rashidromana05@gmail.com>
2024-10-24 19:48:15 +0600

    1st sheet 3rd problem

---
+++ sheet-1 data type-conditions/C-Simple Calculator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int x,y; //10^5 * 10^5 = 10^10; we know int Range: -2,147,483,648 to 2,147,483,647
+    cin>>x>>y;
+    cout<<x<<" + "<<y<<" = "<<x+y<<endl;
+    cout<<x<<" * "<<y<<" = "<<x*y<<endl;
+    cout<<x<<" - "<<y<<" = "<<x-y<<endl;
+}
